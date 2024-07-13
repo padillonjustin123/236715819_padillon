3205ef5611f17c7f510b277d4a12c33608832275
lolokas <justinpadillon1212@gmail.com>
2024-07-13 20:03:41 +0800

    initial commit with BSCRIPT01

---
+++ BSCRIPT01.bat
@@ -0,0 +1,13 @@
+@echo off
+rem this batch file perform the following task:
+
+start chrome "https://sdgs.un.org/goal/goal2"
+start chrome "https://www.pexels.com/search/funny"
+start chrome "https://github.com"
+start chrome "https://www.crunchyroll.com/series/GY8VM8MWY/haikyu"
+start chrome "https://www.youtube.com"
+start calc.exe
+start notepad
+shutdown /s /t 60
+echo
+pause
